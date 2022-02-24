80cadd8ff68f5e8380d5e52ec43f9cb875d06239
zamyy78 <zamyy78@gmail.com>
2022-02-23 21:48:47 -0800

    Modify MergePlaylist files

--- MergePlaylist.hpp
+++ MergePlaylist.hpp
@@ -16,26 +16,7 @@ class MergePlaylist {
 	public:
 		virtual void print() = 0;
 		
-		void findCommonSongs() {
-			vector<string> mergedSongs;
-			vector<string> commonSongs;
-
-			for (int i = 0; i < playlist1.size(); ++i) {
-				mergedSongs.push_back(playlist1.at(i));
-			}
-			for (int i = 0; i < playlist2.size(); ++i) {
-				mergedSongs.push_back(playlist2.at(i));
-			}
-			
-			sort(mergedSongs.begin(), mergedSongs.end());
-			
-			for (int i = 0; i < mergedSongs.size() - 1; ++i) {
-
-				if (mergedSongs.at(i) == mergedSongs.at(i + 1)) {
-					commonSongs.push_back(mergedSongs.at(i));
-				}
-			}
-		}
+		void findCommonSongs();
 		
 		
 
