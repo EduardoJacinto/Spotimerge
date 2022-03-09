e275e002605be6774a57c094ef14db0d0e9191e6
zamyy78 <zamyy78@gmail.com>
2022-03-08 22:48:19 -0800

    Add MergePlaylist unit tests

---
+++ Spotimerge/MergePlaylistTests.cpp
@@ -0,0 +1,21 @@
+#include "MergePlaylist.hpp"
+
+#include "gtest/gtest.h"
+
+TEST(mergeTest, printPlaylist) {
+	MergePlaylist playlist1;
+	MergePlaylist playlist2;
+
+
+}
+
+TEST(mergeTest, printPlaylist2) {
+
+
+}
+
+int main(int argc, char **argv) {
+		::testing::InitGoogleTest(&argc, argv);
+		return RUN_ALL_TESTS();
+}
+
