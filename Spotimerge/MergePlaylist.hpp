48f2afee844559a1ed66db63344ac9fd9c03bc85
zamyy78 <zamyy78@gmail.com>
2022-03-08 22:34:18 -0800

    Modify MergePlaylist

---
+++ Spotimerge/MergePlaylist.hpp
@@ -0,0 +1,26 @@
+#ifndef _MERGE_PLAYLIST_HPP
+#define _MERGE_PLAYLIST_HPP
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class MergePlaylist : public MainWindow {
+	private:
+		vector<string> playlist1;
+		vector<string> playlist2;
+		vector<string> commonSongsList;
+	public:
+		virtual void print() = 0;
+		
+		void findCommonSongs();
+		
+		
+
+
+};
+
+#endif //_MERGE_PLAYLIST_HPP
