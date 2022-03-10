f12308106b4c2c0e13af6612654f5f2025c7f153
Amy Zheng <56276783+zamyy78@users.noreply.github.com>
2022-03-10 00:56:52 -0800

    Update MergePlaylistTests.cpp

--- Spotimerge/MergePlaylistTests.cpp
+++ Spotimerge/MergePlaylistTests.cpp
@@ -2,14 +2,73 @@
 
 #include "gtest/gtest.h"
 
-TEST(mergeTest, printPlaylist) {
-	MergePlaylist playlist1;
-	MergePlaylist playlist2;
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+TEST(mergeTest, checkNotEmptyMergedPlaylist) {
+        MergePlaylist test;
+        vector<string> playlist1 = {"Panda - Desiigner", "Cheap Thrills - Sia","One Dance - Drake", "Diamonds - Rihanna",
+        "Famous - Kanye West"};
+        vector<string> playlist2 = {"Cheap Thrills - Sia", "Panda - Desiigner", "One Dance - Drake",
+        "Work from Home - Fifth Harmony", "NEUNEU - Uffie"};
+        vector<string> playlist3;
+
+        test.findCommonSongs(playlist1, playlist2, playlist3);
+
+        for (int i = 0; i < playlist3.size(); ++i) {
+		cout << playlist3.at(i) << endl;
+	}
 
 }
 
-TEST(mergeTest, printPlaylist2) {
+
+TEST(mergeTest, checkMergedPlaylist1) {
+	MergePlaylist test;
+	vector<string> playlist1 = {"Panda - Desiigner", "Cheap Thrills - Sia","One Dance - Drake", "Diamonds - Rihanna",
+	"Famous - Kanye West"};
+	vector<string> playlist2 = {"Cheap Thrills - Sia", "Panda - Desiigner", "One Dance - Drake", 
+	"Work from Home - Fifth Harmony", "NEUNEU - Uffie"};
+	vector<string> playlist3;
+
+	vector<string> expectedPlaylist = {"Panda - Desiigner", "Cheap Thrills - Sia", "One Dance - Drake"};
+
+	test.findCommonSongs(playlist1, playlist2, playlist3);
+
+	EXPECT_EQ(playlist3,expectedPlaylist);
+
+}
+
+TEST(mergeTest, checkMergedPlaylist2) {
+	MergePlaylist test;
+	vector<string> playlist1 = {"Panda - Desiigner", "Cheap Thrills - Sia", "Diamonds - Rihanna", "Famous - Kanye West"};
+	vector<string> playlist2 = {"Panda - Desiigner", "Cheap Thrills - Sia", "One Dance - Drake"};
+
+	vector<string> playlist3;
+
+	vector<string> expectedPlaylist = {"Panda - Desiigner", "Cheap Thrills - Sia"};
+
+	test.findCommonSongs(playlist1, playlist2, playlist3);
+
+	EXPECT_EQ(playlist3, expectedPlaylist);
+
+}
+
+TEST(mergeTest, playlistSameSongName) {
+	MergePlaylist test;
+
+	vector<string> playlist1 = {"Panda - Josh Levi", "Cheap Thrills - Sia"};
+        vector<string> playlist2 = {"Panda - Desiigner", "Cheap Thrills - Sia", "One Dance - Drake"};
+
+        vector<string> playlist3;
+
+        vector<string> expectedPlaylist = {"Cheap Thrills - Sia"};
+
+        test.findCommonSongs(playlist1, playlist2, playlist3);
+
+        EXPECT_EQ(playlist3, expectedPlaylist);
 
 
 }
